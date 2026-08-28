3a88834ec09e4cedb2ccc2dc24bff9ed7c769266
海葉なっふ <743867237@qq.com>
2026-08-28 23:39:10 +0800

    Add files via upload

---
+++ app/src/main/java/com/wallpaperextend/processor/WallpaperConfig.kt
@@ -0,0 +1,10 @@
+package com.wallpaperextend.processor
+
+data class WallpaperConfig(
+    val blurRadius: Float = 25f,
+    val extendRatio: Float = 0.35f,
+    val featherWidth: Int = 200,
+    val saturationBoost: Float = 1.1f,
+    val brightnessOffset: Float = 0.05f,
+    val overlayStrength: Float = 0.08f
+)
