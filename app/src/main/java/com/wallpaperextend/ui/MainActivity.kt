27be5bc3c50ed81a6fb6604f2d22b4fee2f5eeb4
海葉なっふ <743867237@qq.com>
2026-08-28 17:43:10 +0800

    Add coroutine imports to MainActivity

--- app/src/main/java/com/wallpaperextend/ui/MainActivity.kt
+++ app/src/main/java/com/wallpaperextend/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.wallpaperextend.ui
 
+import kotlin.coroutines.coroutineContext
+import kotlinx.coroutines.ensureActive
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
